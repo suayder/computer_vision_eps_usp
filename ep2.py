81d0efbbb02a01a5055713a98fc0cfe8e2fca225
DavidCadtec <david@cadtec.com.br>
2021-06-27 01:35:53 -0300

    Implement GradSum em img_transformers
    Implementação para testes finais da classe GradientSum() em Img_transformers.py

--- ep2.py
+++ ep2.py
@@ -4,24 +4,20 @@ from src.augmenter import Transform, Augmenter
 from src.transform.img_transformers import Rgb2Gray, Original, ImageEqualization
 from src.normalize import Normalize
 
-base_path = '/home/suayder/Desktop/visao/data_prep/dataset/data'
+base_path = '/home/David_PRO/MAC5768_Visao_Computacional/dataset/data'
 
 trasnformations = Transform.compose([Rgb2Gray(), Original()])
 data_augmenter = Augmenter(base_path,
                            os.path.join(base_path,'metadata.csv'),
                            trasnformations)
 
-#augmented = data_augmenter.process_item('7.jpg')
-#data_augmenter.process_dataset_and_save()
-#image, _ = data.get_item('7.jpg')
-#data.show_item('7.jpg')
-
+augmented = data_augmenter.process_item('472.JPEG')
+data_augmenter.process_dataset_and_save()
+image, _ = data.get_item('472.JPEG')
+data.show_item('472.JPEG')
 
 ## HISTOGRAM EQUALIZATION
 
-histogram_transform = Transform.compose([ImageEqualization()])
-data_normalizer = Normalize(base_path,
-                            os.path.join(base_path,'metadata.csv'),
-                            histogram_transform)
-
-transformed = data_normalizer.show_image('7.jpg')
+#histogram_transform = Transform.compose([ImageEqualization()])
+#data_normalizer = Normalize(base_path,os.path.join(base_path,'metadata.csv'),histogram_transform)
+# transformed = data_normalizer.show_image('472.JPEG')
--- src/transform/img_transformers.py
+++ src/transform/img_transformers.py
@@ -29,15 +29,35 @@ class GradientSum(object):
     """
     descrição do exercício (que está no edisciplinas): Soma de fundo com gradiente de níveis de cinza
     """
-    def __init__(self, name = 'GradSum') -> None:
+    def __init__(self, dGrad, name = 'GradSum') -> None:
         """
+        :param dGrad: define a direção de aplicação do gradiente de sombras na imagem
+            0 - Gradiente na Direção Vertical
+            1 - Gradiente na Direção Horizontal
+            2 - Gradiente na Direção Diagonal
         """
+        self.dGrad = dGrad
         self.name = name
 
     def __call__(self, image:np.ndarray) -> np.ndarray:
         
-        grad_image = image 
-        return grad_image
+        imgProcess = np.zeros(image.shape, dtype = "uint8")
+        
+        if (self.dGrad == 0):
+            for lin in range(0,image.shape[0]-1):
+                imgProcess[lin,:] = image[lin,:] * float(lin/image.shape[0])
+
+        if (self.dGrad == 1):
+            for col in range(0,image.shape[1]-1):
+                imgProcess[:, col] = image[:,col] * float(col/image.shape[1])
+
+        if (self.dGrad == 2):
+            for lin in range(0,image.shape[0]-1):
+                for col in range(0,image.shape[1]-1):
+                    imgProcess[lin, col] = image[lin,col] * (float(lin/image.shape[0]) + float(col/image.shape[1]))/2
+
+        return imgProcess
+
  
 #https://scikit-image.org/docs/dev/api/skimage.exposure.html#skimage.exposure.adjust_log
 
